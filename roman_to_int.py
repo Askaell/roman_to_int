865ee99af51e293981b30dffbf9c874b092bcea2
Askael <andrey.b176@gmail.com>
2020-11-17 10:09:21 +0300

    algorithm roman to int is done and add extra tests

--- roman_to_int.py
+++ roman_to_int.py
@@ -1,12 +1,38 @@
 class Solution:
      def romanToInt(self, s: str) -> int:
-        if len(s) < 1 or len(s) > 15:
+        if (len(s) < 1) or (len(s) > 15):
             return None
-        
+        s = s.upper()
+
         romanAlphabet = {
             'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
         }
 
-        return 0
+        output = 0
+        currentValue = 0
+        middleValue = 0
+        
+        for key in s[::-1]:
+            previousValue = currentValue
+            currentValue = romanAlphabet[key]
 
-print(Solution().romanToInt("1234567891234567"))
+            if previousValue == 0:
+                middleValue = currentValue       
+            else:
+                if previousValue == currentValue:
+                    middleValue += currentValue
+
+                elif previousValue > currentValue:
+                    output += previousValue - currentValue
+                    currentValue = 0
+                    middleValue = 0
+                
+                elif previousValue < currentValue:
+                    output += currentValue + middleValue
+                    currentValue = 0
+                    middleValue = 0
+        
+        if middleValue != 0:
+            output += middleValue
+
+        return output
--- test_roman_to_int.py
+++ test_roman_to_int.py
@@ -1,42 +1,66 @@
-import roman_to_int as rToI
+from roman_to_int import Solution
 
 class TestCases():
     def test_case1(self):
         s='III'
         expected = 3
 
-        result = rToI.Solution().romanToInt(s)
+        result = Solution().romanToInt(s)
 
-        assert result == expected, f"Had: {s} ; Expected: {expected} ; Result: {result}"
+        assert expected == result, f"Had: {s} ; Expected: {expected} ; Result: {result}"
 
-    # def test_case2(self):
-    #     s='IV'
-    #     expected = 4
+    def test_case2(self):
+        s='IV'
+        expected = 4
         
-    #     result = rToI.Solution().romanToInt(s)
+        result = Solution().romanToInt(s)
 
-    #     assert result == expected, f"Had: {s} ; Expected value: {expected} ; Result value: {result}"
+        assert expected == result, f"Had: {s} ; Expected value: {expected} ; Result value: {result}"
 
-    # def test_case3(self):
-    #     s='IX'
-    #     expected = 9
+    def test_case3(self):
+        s='IX'
+        expected = 9
         
-    #     result = rToI.Solution().romanToInt(s)
+        result = Solution().romanToInt(s)
 
-    #     assert result == expected, f"Had: {s} ; Expected value: {expected} ; Result value: {result}"
+        assert expected == result, f"Had: {s} ; Expected value: {expected} ; Result value: {result}"
 
-    # def test_case4(self):
-    #     s='LVIII'
-    #     expected = 58
+    def test_case4(self):
+        s='LVIII'
+        expected = 58
         
-    #     result = rToI.Solution().romanToInt(s)
+        result = Solution().romanToInt(s)
 
-    #     assert result == expected, f"Had: {s} ; Expected value: {expected} ; Result value: {result}"
+        assert expected == result, f"Had: {s} ; Expected value: {expected} ; Result value: {result}"
 
-    # def test_case5(self):
-    #     s='MCMXCIV'
-    #     expected = 1994
+    def test_case5(self):
+        s='MCMXCIV'
+        expected = 1994
         
-    #     result = rToI.Solution().romanToInt(s)
+        result = Solution().romanToInt(s)
 
-    #     assert result == expected, f"Had: {s} ; Expected value: {expected} ; Result value: {result}"
+        assert expected == result, f"Had: {s} ; Expected value: {expected} ; Result value: {result}"
+
+    def test_case6(self):
+        s=''
+        expected = None
+        
+        result = Solution().romanToInt(s)
+
+        assert expected == result, f"Had: {s} ; Expected value: {expected} ; Result value: {result}"
+    
+    def test_case7(self):
+        s='I'
+        expected = 1
+        
+        result = Solution().romanToInt(s)
+
+        assert expected == result, f"Had: {s} ; Expected value: {expected} ; Result value: {result}"
+    
+    def test_case8(self):
+        s='MMMDCCCCXCIX'
+        expected = 3999
+        
+        result = Solution().romanToInt(s)
+
+        assert expected == result, f"Had: {s} ; Expected value: {expected} ; Result value: {result}"
